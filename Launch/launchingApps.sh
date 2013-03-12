4a09434f01f6249f9650d34ed8efb57d90b412e5
Aurelien <clavelin.aurelien@gmail.com>
2013-03-12 16:35:24 +0100

    Ajout du module launch

---
+++ Launch/launchingApps.sh
@@ -0,0 +1,28 @@
+# Script for launching apps
+# RICM5 - IDS
+# http://air.imag.fr/mediawiki/index.php/Projet_2013_:_Interactive_Digital_Signage
+
+
+# Go on 1st desktop
+python2.7 switch_desktop up
+python2.7 switch_desktop left
+
+# Start EDTdemon
+
+
+# StartKiCtrl
+
+
+# Start pdfViewer
+
+
+# Go on 2nd desktop
+sleep 1
+python2.7 switch_desktop down
+
+# Start macScanner
+sh ../macScanner/main.sh &
+
+# Go back on 1st desktop
+sleep 5
+python2.7 switch_desktop up
