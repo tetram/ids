551ad05573090fc516455cc370133691c4f5b42e
RICMonMacMini <RICMonMacMini@e.ujf-grenoble.fr>
2013-03-13 15:24:29 +0100

    kinect frame

--- kinect/src/gesture/GestureDetect.java
+++ kinect/src/gesture/GestureDetect.java
@@ -87,6 +87,12 @@ public class GestureDetect
 
   public GestureDetect()
   {
+//	    JFrame jf = new JFrame();
+//	    jf.setSize(500, 500);
+//	    jf.setVisible(true);
+//	    new SimpleViewerApplication(new JFrame());
+//	    new Sim
+	    
 	  try {
 		rob = new Robot();
 	} catch (AWTException e1) {
@@ -111,7 +117,7 @@ public class GestureDetect
       e.printStackTrace();
     }
     
-    new SimpleViewerApplication(new JFrame());
+
     
   }  // end of GestureDetect()
 
