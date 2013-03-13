db03ab9393c83e266134f7db215faef343da7625
Aurelien <nomail@mail.com>
2013-03-13 16:08:20 +0100

    Lauch script modifie

--- Launch/launchingApps.sh
+++ Launch/launchingApps.sh
@@ -1,28 +1,29 @@
 # Script for launching apps
 # RICM5 - IDS
 # http://air.imag.fr/mediawiki/index.php/Projet_2013_:_Interactive_Digital_Signage
-
-
-# Go on 1st desktop
-python2.7 switch_desktop up
-python2.7 switch_desktop left
-
-# Start EDTdemon
-
-
-# StartKiCtrl
-
-
-# Start pdfViewer
+#
+# This script assume that there are only too active desktops on your envirronment (one left and one right)
 
 
 # Go on 2nd desktop
-sleep 1
-python2.7 switch_desktop down
+python2.7 switch_desktop right
 
-# Start macScanner
+# Start macScanner on 2nd desktop
 sh ../macScanner/main.sh wlan1 &
 
-# Go back on 1st desktop
+# Go on 1st desktop
 sleep 5
-python2.7 switch_desktop up
+python2.7 switch_desktop left
+
+# Start EDTdemon on 1st desktop
+#java -jar ../EDTDemon/EDTDemons.jar
+
+# StartKiCtrl on 1st desktop
+
+
+# Start pdfViewer on 1st desktop
+python2.7 ../PicViewer/print_pic.py
+
+
+
+
