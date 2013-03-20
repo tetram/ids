a88be300367ca0579addf73880c605b60be0cef8
RICMonMacMini <RICMonMacMini@e.ujf-grenoble.fr>
2013-03-20 09:48:06 +0100

    sec moins fort

--- macScanner/boucle.sh
+++ macScanner/boucle.sh
@@ -1,7 +1,7 @@
 # Boucle d'execution du parsage !
 while true; do
 		echo start
-		python ./python/csvparser.py capture-01.csv 200000
+		python ./python/csvparser.py capture-01.csv 1800
 		echo end
 		sleep 2
 	done
