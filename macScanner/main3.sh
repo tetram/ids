ec24c057f18d53e4cd749dacff83e577979784e9
Aurelien <clavelin.aurelien@gmail.com>
2013-03-20 10:44:51 +0100

    Reindentation main.sh dans main3.sh

---
+++ macScanner/main3.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+########################################
+#
+# Permet de lancer le monitoring des wifi
+# actifs
+#
+########################################
+
+bashtrap() {
+    echo "CTRL+C Detected !...executing bash trap !"
+    # fin
+    # TODO stopper airodump et le java ?
+    airmon-ng stop mon0
+    rm -rf capture*
+}
+
+# Definition de la focntion appelee après un CTRL+C
+trap bashtrap INT
+if [[ $EUID -ne 0 ]]
+    then
+        echo "Please run the script as root !"
+        exit 1
+fi
+if [ $# = 1 ]
+    then
+        # Passer l'interface en mode monitoring
+        airmon-ng start $1
+        # Effacer le fichier out si existant
+        rm -rf capture*
+        # Ecriture de la sortie dans le fichier capture
+        gnome-terminal -e 'airodump-ng -w capture mon0' &
+        sleep 10
+        # Execution
+        sh boucle.sh | java -jar java/SinfageMac.jar
+    else
+        echo "Syntaxe : $0 interface"
+fi
+
